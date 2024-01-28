cc57e9e0765140eec28a51d838926b8c0fff26cf
Manaswini Sasmal <98816752+Monisasmal@users.noreply.github.com>
2024-01-28 14:47:06 +0530

    Update index.js
    Add Hash Router

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import { AppProvider } from './context/productcontext';
 import { FilterProviderContext } from './context/filterContext';
 import {CartProvider} from './context/cartContext';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { HashRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <HashRouter>
   <Auth0Provider
     domain="dev-ie3ypbqs6izl0f3m.us.auth0.com"
     clientId="JmoCYNBsshzzrCjXgAnCzDqkux6w0J9t"
@@ -25,6 +27,6 @@ root.render(
     </FilterProviderContext>
     </AppProvider>
     </Auth0Provider>
-  
+  </HashRouter>
 );
 
