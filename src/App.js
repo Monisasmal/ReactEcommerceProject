a0d5eafc855e7431f717064399480a5d01714d3a
Manaswini Sasmal <98816752+Monisasmal@users.noreply.github.com>
2024-01-28 17:05:34 +0530

    Update App.js
    1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route} from "react-router-dom";
+import { HashRouter,Routes, Route} from "react-router-dom";
 import Home from './Home';
 import About from './About';
 import Products from './Products';
